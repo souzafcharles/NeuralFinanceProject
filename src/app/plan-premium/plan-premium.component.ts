e3c424e69aca3226fc3a6cfe07635f1cd38aed4f
souzafcharles <souzach@outlook.com>
2023-05-29 19:07:16 -0300

    Update files

--- src/app/plan-premium/plan-premium.component.ts
+++ src/app/plan-premium/plan-premium.component.ts
@@ -6,5 +6,7 @@ import { Component } from '@angular/core';
   styleUrls: ['./plan-premium.component.less']
 })
 export class PlanPremiumComponent {
-
+  openColab() {
+    window.open('https://colab.research.google.com/drive/1sdB18BDazs2KAT5wUU9wwG8v7NDHbhuE', '_blank');
+  }
 }
