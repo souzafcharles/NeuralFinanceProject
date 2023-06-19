16a7088a93e59f697799fcb3865164dce1011f2c
thiagoVilelaGerolamo <thiago.v.gerolamo@gmail.com>
2023-06-19 19:10:05 -0300

    add o ultimo envio

--- src/app/plan-free/plan-free.component.ts
+++ src/app/plan-free/plan-free.component.ts
@@ -6,5 +6,7 @@ import { Component } from '@angular/core';
   styleUrls: ['./plan-free.component.less']
 })
 export class PlanFreeComponent {
-
+  openColab() {
+    window.open('https://colab.research.google.com/drive/1sdB18BDazs2KAT5wUU9wwG8v7NDHbhuE', '_blank');
+  }
 }
